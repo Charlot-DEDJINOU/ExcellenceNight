2f3acd958cc13cca56bab800c381de2c7f28b798
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2023-12-12 23:08:09 +0100

    modified

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,9 @@
 <script setup>
-
+import VisuelNight from './components/Visuel-Night.vue'
 </script>
 
 <template>
-  <div>
-    Momo
-  </div>
+  <VisuelNight />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
